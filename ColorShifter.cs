9323254528e69daf9a96aa0f008b0bb1d20e5601
Sean <seantdwilson@gmail.com>
2014-07-28 22:37:21 -0700

    Uploaded first version of ColorShifter.cs

---
+++ ColorShifter.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+namespace Color_Shifter
+{
+    class ColorShifter
+    {
+        [DllImport("dwmapi.dll", EntryPoint = "#127")]
+        public static extern void DwmGetColorizationParameters(ref ColorizationParameters c);
+
+        [DllImport("dwmapi.dll", EntryPoint = "#131")]
+        public static extern void DwmSetColorizationParameters(ref ColorizationParameters c, int needed = 0);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ColorizationParameters
+        {
+            public int color, afterglow, colorBalance, afterglowBalance, blurBalance, glassReflectionIntensity, opaqueBlend;
+        }
+    }
+}
